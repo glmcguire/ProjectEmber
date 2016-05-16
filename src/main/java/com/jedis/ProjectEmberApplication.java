eb988dec7a2e8cbe76ca6caba7e404025229eef4
glmcguire <glmcguir@yahoo.com>
2016-05-16 09:26:28 -0700

    Initial commit

---
+++ src/main/java/com/jedis/ProjectEmberApplication.java
@@ -0,0 +1,23 @@
+package com.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Sample application to connect to both MySql DB and Redis; utilizing
+ * Jedis to copy DB into Redis cache.
+ * 
+ * 
+ * @author Gary McGuire and Sean Clements
+ *
+ */
+
+@SpringBootApplication
+public class ProjectEmberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEmberApplication.class, args);
+	}
+	
+	//TODO Jedis connection to come. 
+}
